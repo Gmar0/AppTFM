bd8181c263854c7f8456136225f1e99e36fb4faa
MSI-914\Gemma <gmaro@usal.es>
2025-02-08 00:39:58 +0100

    Upload ExitGame with BackButton

--- Assets/Scripts/ExitManager.cs
+++ Assets/Scripts/ExitManager.cs
@@ -12,7 +12,7 @@ public class ExitManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    /*void Update()
     {
         if(Input.GetKeyDown(KeyCode.Escape))
         {
@@ -21,5 +21,34 @@ public class ExitManager : MonoBehaviour
             else
                 SceneManager.LoadScene(0);
         }
-    }
+    }*/
+
+[SerializeField] GameObject exitPanel;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (SceneManager.GetActiveScene ().buildIndex != 0) {
+				SceneManager.LoadScene (0);
+			} else {
+				if (exitPanel) {
+					exitPanel.SetActive (true);
+				}
+			}
+		}
+	}
+
+	public void onUserClickYesNo (int choice)
+	{//choice==0 no     choice==1 yes
+		if (choice == 1) {
+			Application.Quit ();
+		}
+		exitPanel.SetActive (false);//else
+	}
+
+	public void onUserClickStart ()
+	{
+		SceneManager.LoadScene (1);//load game scene
+	}
+
 }

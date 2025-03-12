688fa863240b2aac2040d56f378e570456757d7c
MSI-914\Gemma <gmaro@usal.es>
2025-03-12 10:50:59 +0100

    FixScanningChanges

--- Assets/Scripts/IconManager.cs
+++ Assets/Scripts/IconManager.cs
@@ -3,53 +3,69 @@ using UnityEngine.UI;
 
 public class IconManager : MonoBehaviour
 {
-    [Header("ICONO 4 POSTES\n")]
+    [Header("PIN ICON 4 POSTES\n")]
     public string nameModel4Postes;
     [SerializeField] Image icon4Postes;
     [SerializeField] Sprite modeloVisto_4P;
 
 
-    [Header("ICONO SAN PEDRO\n")]
+    [Header("PIN ICON SAN PEDRO\n")]
     public string nameModelSanPedro;
     [SerializeField] Image iconSanPedro;
     [SerializeField] Sprite modeloVisto_SP;
 
-    [Header("ICONO ADOLFO SUAREZ\n")]
+    [Header("PIN ICON ADOLFO SUAREZ\n")]
     public string nameModelAdolfo;
     [SerializeField] Image iconAdolfo;
     [SerializeField] Sprite modeloVisto_A;
 
-    [Header("ICONO VERRACO 1\n")]
+    [Header("IMAGEN 4 POSTES\n")]
+    //public string nameModel4P_Image;
+    [SerializeField] Image image4Postes;
+    [SerializeField] Sprite imageVista_4P;
+
+
+    [Header("IMAGEN SAN PEDRO\n")]
+    //public string nameModelSP_Image;
+    [SerializeField] Image imageSanPedro;
+    [SerializeField] Sprite imageVista_SP;
+
+    [Header("IMAGEN ADOLFO SUAREZ\n")]
+    //public string nameModelA_Image;
+    [SerializeField] Image imageAdolfo;
+    [SerializeField] Sprite imageVista_A;
+
+    [Header("PIN ICON VERRACO 1\n")]
     public string nameModelVerraco1;
     [SerializeField] Image iconVerraco1;
     [SerializeField] Sprite modeloVisto_V1;
 
-    [Header("ICONO VERRACO 2\n")]
+    [Header("PIN ICON VERRACO 2\n")]
     public string nameModelVerraco2;
     [SerializeField] Image iconVerraco2;
     [SerializeField] Sprite modeloVisto_V2;
 
-    [Header("ICONO VERRACO 3\n")]
+    [Header("PIN ICON VERRACO 3\n")]
     public string nameModelVerraco3;
     [SerializeField] Image iconVerraco3;
     [SerializeField] Sprite modeloVisto_V3;
 
-    [Header("ICONO VERRACO 4\n")]
+    [Header("PIN ICON VERRACO 4\n")]
     public string nameModelVerraco4;
     [SerializeField] Image iconVerraco4;
     [SerializeField] Sprite modeloVisto_V4;
 
-    [Header("ICONO VERRACO 5\n")]
+    [Header("PIN ICON VERRACO 5\n")]
     public string nameModelVerraco5;
     [SerializeField] Image iconVerraco5;
     [SerializeField] Sprite modeloVisto_V5;
 
-    [Header("ICONO VERRACO 6\n")]
+    [Header("PIN ICON VERRACO 6\n")]
     public string nameModelVerraco6;
     [SerializeField] Image iconVerraco6;
     [SerializeField] Sprite modeloVisto_V6;
 
-    [Header("ICONO VERRACO 7\n")]
+    [Header("PIN ICON VERRACO 7\n")]
     public string nameModelVerraco7;
     [SerializeField] Image iconVerraco7;
     [SerializeField] Sprite modeloVisto_V7;
@@ -63,20 +79,23 @@ public class IconManager : MonoBehaviour
 
         if (PlayerPrefs.GetInt(nameModel4Postes.ToLower(), 0) != 0)
         {
-            icon4Postes.color = Color.white;
+            //icon4Postes.color = Color.white;
             icon4Postes.sprite = modeloVisto_4P;
+            image4Postes.sprite = imageVista_4P;
         }
 
         if (PlayerPrefs.GetInt(nameModelSanPedro.ToLower(), 0) != 0)
         {
             iconSanPedro.color = Color.white;
             iconSanPedro.sprite = modeloVisto_SP;
+            imageSanPedro.sprite = imageVista_SP;
         }
 
         if (PlayerPrefs.GetInt(nameModelAdolfo.ToLower(), 0) != 0)
         {
             iconAdolfo.color = Color.white;
             iconAdolfo.sprite = modeloVisto_A;
+            imageAdolfo.sprite = imageVista_A;
         }
 
         if (PlayerPrefs.GetInt(nameModelVerraco1.ToLower(), 0) != 0)
